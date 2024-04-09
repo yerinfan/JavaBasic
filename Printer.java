489353a8256257fb039f0a031a72981f80d25c5f
Lee YeEun <78542145+yerinfan@users.noreply.github.com>
2024-04-09 18:04:29 +0900

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,56 @@
+package chap04;
+
+public class Printer {
+	private int numOfPapers;
+	private boolean duplex;
+	
+	
+	void print(int amount) {
+		
+		if (getDuplex()) { // 만약에 양면이면 amount/2 개 출력
+			amount = (amount % 2 == 0) ? amount / 2 : amount / 2 + 1;
+		}
+		
+		
+		//용지 없음
+		if (isEmpty()) {
+			System.out.println("용지가 없습니다.");
+			return;
+		}
+		//용지 충분
+		else if (numOfPapers >= amount) {
+			this.numOfPapers -= amount;
+			System.out.printf("%d장 출력했습니다.\n현재 %d장 남아있습니다.\n", amount, numOfPapers);
+		}
+		
+		//용지 부족
+		else {
+			System.out.printf("모두 출력하려면 용지가 %d장 부족합니다.\t%d장만 출력합니다.\n", amount - numOfPapers, numOfPapers);
+			this.numOfPapers = 0;
+		}
+		
+	}
+
+
+	 public boolean getDuplex() {
+		 return duplex;
+	 }
+	 
+	public void setDuplex(boolean duplex) {
+		this.duplex = duplex;
+	}
+
+	public Printer(int numOfPapers, boolean duplex) {
+		this.numOfPapers = numOfPapers;
+		this.duplex = duplex;
+	}
+	
+	 public boolean isEmpty() {
+		 if (numOfPapers == 0) return true;
+		 
+		 return false;
+	 }
+	
+	
+	
+}
---
+++ PrinterTest.java
@@ -0,0 +1,12 @@
+package chap04;
+
+public class PrinterTest {
+	public static void main(String[] args) {
+		/*Printer p = new Printer(10);
+		p.print(2);
+		p.print(20);
+		p.print(10);
+		*/
+		
+	}
+}
