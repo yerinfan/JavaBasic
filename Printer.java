73f346119587f632c071d9ff4a45fd2e56561c57
Lee YeEun <78542145+yerinfan@users.noreply.github.com>
2024-04-10 15:33:50 +0900

    Add files via upload

--- Printer.java
+++ Printer.java
@@ -17,12 +17,12 @@ public class Printer {
 			//용지 충분
 			else if (numOfPapers >= amount) {
 				this.numOfPapers -= amount;
-				System.out.printf("양면으로 %d장 출력했습니다.\n현재 %d장 남아있습니다.\n", amount, numOfPapers);
+				System.out.printf("양면으로 %d장 출력했습니다. 현재 %d장 남아있습니다.\n", amount, numOfPapers);
 			}
 			
 			//용지 부족
 			else {
-				System.out.printf("모두 출력하려면 용지가 %d장 부족합니다. 양면으로\t%d장만 출력합니다.\n", amount - numOfPapers, numOfPapers);
+				System.out.printf("모두 출력하려면 용지가 %d장 부족합니다. 양면으로 %d장만 출력합니다.\n", amount - numOfPapers, numOfPapers);
 				this.numOfPapers = 0;
 			}
 		}
@@ -36,12 +36,12 @@ public class Printer {
 		//용지 충분
 		else if (numOfPapers >= amount) {
 			this.numOfPapers -= amount;
-			System.out.printf("단면으로 %d장 출력했습니다.\n현재 %d장 남아있습니다.\n", amount, numOfPapers);
+			System.out.printf("단면으로 %d장 출력했습니다. 현재 %d장 남아있습니다.\n", amount, numOfPapers);
 		}
 		
 		//용지 부족
 		else {
-			System.out.printf("모두 출력하려면 용지가 %d장 부족합니다.단면으로 \t%d장만 출력합니다.\n", amount - numOfPapers, numOfPapers);
+			System.out.printf("모두 출력하려면 용지가 %d장 부족합니다. 단면으로 %d장만 출력합니다.\n", amount - numOfPapers, numOfPapers);
 			this.numOfPapers = 0;
 		}
 		
